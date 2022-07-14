6d43678cc926223e53239be5d47a3fa37fbc5c78
Herdina <gherdina7@gmail.com>
2022-07-14 03:00:31 -0300

    Preparado para gh-pages

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux';
-import './index.css';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={ store }>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </Provider>,
